8f0dd298263dcbd01fb029e9d0988a04207d8340
sunguanxiong <sunguanxiong@baidu.com>
2017-08-10 09:56:45 +0800

    ADD TESTING

--- mnist.py
+++ mnist.py
@@ -15,10 +15,23 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 x = tf.placeholder(tf.float32, shape=[None,784]);
 y_ = tf.placeholder(tf.float32, shape=[None,10]);
 
+#NO RELU;NO regularize
+
+#w1 = tf.Variable(tf.zeros(shape=[784,64]));
+#b1 = tf.Variable(tf.zeros(shape=[1,64]));
+#y1 = tf.matmul(x, w1) + b1;#[N,64]
+#
+#w2 = tf.Variable(tf.zeros([64,64]));
+#b2 = tf.Variable(tf.zeros([1,64]));
+#y_2 = tf.matmul(y1, w2) + b2;#[N,64]
+#
+#w3 = tf.Variable(tf.zeros([64,10]));
+#b3 = tf.Variable(tf.zeros([1,10]));
+#y_p = tf.matmul(y_2, w3) + b3;#[N,10]
+
 w = tf.Variable(tf.zeros(shape=[784,10]));
 b = tf.Variable(tf.zeros(shape=[1,10]));
-
-y_p = tf.matmul(x, w)+b;
+y_p = tf.matmul(x, w) + b;#[N,10]
 
 cross_entropy = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=y_p))
@@ -30,7 +43,7 @@ train_step = optimizer.minimize(cross_entropy);
 sess = tf.InteractiveSession();
 sess.run(tf.global_variables_initializer());
 
-for i in range(1000):
+for i in range(10000):
     
     minibatch = mnist.train.next_batch(100);
     loss, train = sess.run(
@@ -42,5 +55,9 @@ for i in range(1000):
 # Test trained model
 correct_prediction = tf.equal(tf.argmax(y_p, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print("The training accuracy is : %s " %(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                      y_: mnist.test.labels})))
+print("The training accuracy is : %s " %(sess.run(accuracy, 
+                feed_dict={x: mnist.train.images,y_: mnist.train.labels})))
+
+# Print ACCURACY ON TEST DATASET    
+print("The testing accuracy is : %s " %(accuracy.eval(
+                feed_dict={x:mnist.test.images,y_:mnist.test.labels})));
